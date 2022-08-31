7ddd37deccfee0b2588fbb7601b30a3835919398
M3IJI3 <majorangeee@gmail.com>
2022-08-31 14:18:33 -0700

    update files

---
+++ Project5/Project5/main.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//define是一个预处理指令
+
+//1. define定义符号
+//#define MAX 1000
+//int main()
+//{
+//	printf("%d\n", MAX);
+//	return 0;
+//}
+
+//2. define定义宏
+//#define ADD(X,Y) ((X)+(Y)) 
+//int main()
+//{
+//	printf("%d\n", 4 * ADD(2, 3));
+//	return 0;
+//}
